4dcd1e972a53beed6b7d719bb4eaec34fa52cc00
kimyw2353 <kimyw2353@naver.com>
2022-11-01 17:25:35 +0900

    입력 폼 작성

---
+++ create.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport"
+	      content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<title>create</title>
+</head>
+<body>
+<h1>WEB</h1>
+<ol>
+	<li>HTML</li>
+</ol>
+<form action="process_create.php" method="post">
+	<p><input type="text" name="title" placeholder="title"></p>
+	<p><textarea name="description" placeholder="description"></textarea></p>
+    <p><input type="submit"></p>
+</form>
+</body>
+</html>
