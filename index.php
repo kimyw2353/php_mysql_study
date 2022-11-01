529795b8bad3881e1cfdeb72625c313bba211201
kimyw2353 <kimyw2353@naver.com>
2022-11-01 17:25:12 +0900

    생성버튼 추가

--- index.php
+++ index.php
@@ -5,13 +5,14 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>home</title>
 </head>
 <body>
 <h1>WEB</h1>
 <ol>
     <li>HTML</li>
 </ol>
+<a href="create.php">create</a>
 <h2>Welcome</h2>
 </body>
 </html>
