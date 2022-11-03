18bdccddc679863ad8fa14bce734131a2f029eaf
kimyw2353 <kimyw2353@naver.com>
2022-11-03 10:44:30 +0900

    create페이지에 반복문 적용

--- create.php
+++ create.php
@@ -1,16 +1,41 @@
+력<?php
+	$conn = mysqli_connect('localhost', 'root', 'root', 'opentutorials');
+	
+	$sql = "SELECT * FROM topic";
+	$result = mysqli_query($conn, $sql);
+	$list = '';
+	while ($row = mysqli_fetch_array($result)) {
+		$list = $list . "<li><a
+        href=\"index.php?id={$row['id']}\">{$row['title']}
+        </a></li>";
+	}
+	
+	$article = array(
+		'title' => 'Welcome',
+		'description' => 'Hello Yael'
+	);
+	if (isset($_GET['id'])) {
+		$sql = "SELECT * FROM topic WHERE id = {$_GET['id']}";
+		$result = mysqli_query($conn, $sql);
+		$row = mysqli_fetch_array($result);
+		$article['title'] = $row['title'];
+		$article['description'] = $row['description'];
+	}
+
+?>
 <!doctype html>
 <html lang="en">
 <head>
-	<meta charset="UTF-8">
-	<meta name="viewport"
-	      content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-	<meta http-equiv="X-UA-Compatible" content="ie=edge">
-	<title>create</title>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>home</title>
 </head>
 <body>
-<h1>WEB</h1>
+<h1><a href="index.php">WEB</a></h1>
 <ol>
-	<li>HTML</li>
+	<?= $list ?>
 </ol>
 <form action="process_create.php" method="post">
 	<p><input type="text" name="title" placeholder="title"></p>
