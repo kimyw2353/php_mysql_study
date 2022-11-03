648692883ca8d065bad5ee6c3016a4a1113bee25
kimyw2353 <kimyw2353@naver.com>
2022-11-03 10:43:43 +0900

    while문으로 select 예제

--- select.php
+++ select.php
@@ -5,10 +5,46 @@
 		'root',
 		'opentutorials');
 	
-	//$sql = "SELECT * FROM topic"; -- 전체 행 가져오기
-	$sql = "SELECT * FROM topic WHERE id = 11"; //하나의 행만 가져오기
+	/* 1 row */
+	echo '<h1>single row</h1>';
+	$sql = "SELECT * FROM topic WHERE id = 11";
 	$result = mysqli_query($conn, $sql);
 	$row = mysqli_fetch_array($result);
-	print_r($row);
-	echo '<h1>'.$row['title'].'</h1>';
+	echo '<h2>'.$row['title'].'</h2>';
 	echo $row['description'];
+	
+	/* all row */
+	echo '<h1>multi row</h1>';
+	$sql = "SELECT * FROM topic";
+	$result = mysqli_query($conn, $sql);
+	
+//	$row = mysqli_fetch_array($result);
+//	echo '<h2>'.$row['title'].'</h2>';
+//	echo $row['description'];
+//
+//	$row = mysqli_fetch_array($result);
+//	echo '<h2>'.$row['title'].'</h2>';
+//	echo $row['description'];
+//	$row = mysqli_fetch_array($result);
+//	echo '<h2>'.$row['title'].'</h2>';
+//	echo $row['description'];
+//	$row = mysqli_fetch_array($result);
+//	echo '<h2>'.$row['title'].'</h2>';
+//	echo $row['description'];
+//	$row = mysqli_fetch_array($result);
+//	echo '<h2>'.$row['title'].'</h2>';
+//	echo $row['description'];
+//	$row = mysqli_fetch_array($result);
+//	echo '<h2>'.$row['title'].'</h2>';
+//	echo $row['description'];
+	// 더이상 출력할 데이터가 없을 때 null을 반환함. 로직이 바뀌거나 갯수가 바뀌었을 때 출력이 안되는 상황 발생
+	echo '<hr>';
+	
+	/* all row (while) */
+	echo '<h1>while row</h1>';
+	while($row = mysqli_fetch_array($result)){
+		echo '<h2>'.$row['title'].'</h2>';
+		echo $row['description'];
+		//null이 아니라면 참으로 판단해서 계속 실행. null이 나오면 반복문이 종료됨.
+	}
+	
