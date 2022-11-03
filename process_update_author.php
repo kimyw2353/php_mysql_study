835d4279c9d0f9d7a334a14b64f4cc34eb7c217b
kimyw2353 <kimyw2353@naver.com>
2022-11-03 16:50:55 +0900

    author update query 작성

---
+++ process_update_author.php
@@ -0,0 +1,24 @@
+<?php
+	$conn = mysqli_connect('localhost', 'root', 'root', 'opentutorials');
+	
+	settype($_POST['id'], 'integer');
+	$filtered = array(
+		'id'=>mysqli_real_escape_string($conn, $_POST['id']),
+		'name'=>mysqli_real_escape_string($conn, $_POST['name']),
+		'profile'=>mysqli_real_escape_string($conn, $_POST['profile'])
+	);
+	$sql = "
+		UPDATE author
+		SET
+		    name = '{$filtered['name']}',
+		    profile = '{$filtered['profile']}'
+		WHERE
+		    id = {$filtered['id']}
+	";
+	$result = mysqli_query($conn, $sql);
+	if($result === false) {
+		echo '저장하는 중 문제가 생겼슴니다. 관리자에게 문의해주세요.';
+		error_log(mysqli_error($conn));
+	} else {
+		header('Location: author.php?id='.$filtered['id']);
+	}
