b65b2f0b1c57a18db5f4b844187744165a3f3cfe
kimyw2353 <kimyw2353@naver.com>
2022-11-03 14:47:28 +0900

    delete 버튼 추가

--- index.php
+++ index.php
@@ -16,6 +16,7 @@ $article = array(
 	'description' => 'Hello Yael'
 );
 $update_link = '';
+$delete_link = '';
 if (isset($_GET['id'])) {
 	$filteredId = mysqli_real_escape_string($conn, $_GET['id']);
 	$sql = "SELECT * FROM topic WHERE id = {$filteredId}";
@@ -24,26 +25,32 @@ if (isset($_GET['id'])) {
 	$article['title'] = htmlspecialchars($row['title']);
 	$article['description'] = htmlspecialchars($row['description']);
 	$update_link = '<a href="update.php?id='.$_GET['id'].'">update</a>';
+	$delete_link = '
+	    <form action = "process_delete.php" method = "post">
+	        <input type="hidden" name="id" value="'.$_GET['id'].'">
+	        <input type="submit" value="delete">
+		</form>';
 }
 
 ?>
 <!doctype html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>home</title>
-</head>
-<body>
-<h1><a href="index.php">WEB</a></h1>
-<ol>
-	<?=$list?>
-</ol>
-<a href="create.php">create</a>
-<?=$update_link?>
-<h2><?=$article['title']?></h2>
-<?=$article['description']?>
-</body>
+	<head>
+		<meta charset="UTF-8">
+		<meta name="viewport"
+			content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+		<meta http-equiv="X-UA-Compatible" content="ie=edge">
+		<title>home</title>
+	</head>
+	<body>
+		<h1><a href="index.php">WEB</a></h1>
+		<ol>
+			<?=$list?>
+		</ol>
+		<a href="create.php">create</a>
+		<?=$update_link?>
+		<?=$delete_link?>
+		<h2><?=$article['title']?></h2>
+		<?=$article['description']?>
+	</body>
 </html>
