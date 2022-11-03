d4e806446ef370a9ba3184cc19c6145f786ada02
kimyw2353 <kimyw2353@naver.com>
2022-11-03 13:57:21 +0900

    update query추가

---
+++ process_udpate.php
@@ -0,0 +1,24 @@
+<?php
+	$conn = mysqli_connect('localhost', 'root', 'root', 'opentutorials');
+	
+	settype($_POST['id'], 'integer');
+	$filtered = array(
+		'id'=>mysqli_real_escape_string($conn, $_POST['id']),
+		'title'=>mysqli_real_escape_string($conn, $_POST['title']),
+		'description'=>mysqli_real_escape_string($conn, $_POST['description'])
+	);
+	$sql = "
+		UPDATE topic
+		SET
+		    title = '{$filtered['title']}',
+		    description = '{$filtered['description']}'
+		WHERE
+		    id = {$filtered['id']}
+	";
+	$result = mysqli_query($conn, $sql);
+	if($result === false) {
+		echo '저장하는 중 문제가 생겼슴니다. 관리자에게 문의해주세요.';
+		error_log(mysqli_error($conn));
+	} else {
+		echo '수정되었습니다. <a href = "index.php">돌아가기</a>';
+	}
